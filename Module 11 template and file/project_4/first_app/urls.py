8f488775a68fc029a56a16c16a8083594b0c1b97
shihab <shihabshahriar543@gmail.com>
2023-12-07 19:21:57 +0600

    module 11 codes

---
+++ Module 11 template and file/project_4/first_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index , name = 'home'),
+    # path('about/', views.about, name = 'about'),
+    path('about/', views.about, name = 'about'),
+    # path('about/page/<int:id>/', views.about, name = 'about'),
+]
